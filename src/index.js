975bca9847a303cdb67a42ce9f1fd13717b41843
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-12 00:21:24 +0300

    feat: solve 3/14

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let a = Number(first);
+  let b = Number(second);
+  let outputString = String(a * b);
+  return outputString
 }
