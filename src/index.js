7c25e20191a99696bf30c2712f253434b18aad78
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-12 11:09:51 +0300

    feat: solve 14/14

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function multiply(first, second) {
   // your solution
-  let a = Number(first);
-  let b = Number(second);
-  let outputString = String(a * b);
+  let a = BigInt(first); // for operate with huge number > 2^53 -1 use BigInt()
+  let b = BigInt(second); // read more about https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER 
+  let outputString = String(a * b); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
   return outputString
 }
